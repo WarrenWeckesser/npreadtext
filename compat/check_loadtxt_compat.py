e9558d4b2c6cc756a4e0c372f0f2de25bf3945e4
Warren Weckesser <warren.weckesser@gmail.com>
2020-05-27 09:25:45 -0400

    Add a script to run numpy tests with monkey patched loadtxt.

---
+++ compat/check_loadtxt_compat.py
@@ -0,0 +1,61 @@
+
+import argparse
+import textwrap
+import numpy as np
+from readtextstream import read
+
+
+def _loadtxt(*args, **kwds):
+    delimiter = kwds.pop('delimiter', None)
+    if delimiter is None:
+        delimiter = ' '
+
+    dtype = kwds.pop('dtype', None)
+    if dtype is None:
+        dtype = np.float64
+
+    ndmin = kwds.pop('ndmin', None)
+    if ndmin is None:
+        ndmin = 0
+
+    comment = kwds.pop('comments', None)
+    if comment is None:
+        comment = ''
+    elif isinstance(comment, bytes):
+        comment = comment.decode('latin1')
+
+    arr = read(*args, delimiter=delimiter, dtype=dtype, ndmin=ndmin,
+               comment=comment, **kwds)
+    if arr.shape == (0, 0):
+        arr.resize((0,))
+    return arr
+
+
+# Monkey patch numpy.loadtxt
+np.loadtxt = _loadtxt
+
+
+if __name__ == "__main__":
+    descr = textwrap.dedent(
+        """
+        Run numpy tests with loadtxt replaced by a wrapper of the
+        new reader.  Some useful tests to pass as the -t option:
+            numpy.lib.tests.test_regression
+            numpy.lib.tests.test_io::TestLoadTxt
+        """)
+    parser = argparse.ArgumentParser(
+                description=descr,
+                formatter_class=argparse.RawDescriptionHelpFormatter)
+
+    test_help = ('Test to run, using the same syntax as the -t option '
+                 'of runtests.py.')
+    parser.add_argument('-t', '--test', required=True, help=test_help)
+
+    verbose_help = ('Verbosity value for test outputs, in the range 1-3. '
+                    'Default is 1.')
+    parser.add_argument('-v', '--verbose', type=int, choices=[1, 2, 3],
+                        default=1, help=verbose_help)
+
+    args = parser.parse_args()
+
+    np.test(verbose=args.verbose, tests=[args.test])
